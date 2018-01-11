7c5c355c2096dc170d1be5d12963c580966c46de
Jameel Kelley <jamkelley22@gmail.com>
2018-01-11 14:20:44 -0600

    realized my mistake of using cartiesian coordinates...X and Y values are switched now. Attempting tests

--- queens.c
+++ queens.c
@@ -15,18 +15,16 @@ void clearBoard(char a[BOARD_LENGTH][BOARD_LENGTH]);
 bool placeQueen(char board[BOARD_LENGTH][BOARD_LENGTH], int x, int y);
 bool queens(char board[BOARD_LENGTH][BOARD_LENGTH], int x, int y);
 void printSolution(char board[BOARD_LENGTH][BOARD_LENGTH]);
+bool canBeAttacked(char board[BOARD_LENGTH][BOARD_LENGTH],int x, int y);
+void placeQueenR(char board[BOARD_LENGTH][BOARD_LENGTH],int x, int y);
 
 int main(int argc, char *argv[]) {
 	char board[BOARD_LENGTH][BOARD_LENGTH];
 	clearBoard(board);	
-	/*
-	placeQueen(board,1,1);	
-	placeQueen(board,2,4);
-	placeQueen(board,0,7);
-	queens(board,0,0);
-	*/	
+	/*queens(board,0,0);*/
 	placeQueen(board,0,0);
-	placeQueen(board,1,1);
+	placeQueen(board,0,7);
+	placeQueen(board,1,4);
 	printSolution(board);
 	return 0;
 }
@@ -42,79 +40,110 @@ void clearBoard(char a[BOARD_LENGTH][BOARD_LENGTH]) {
 	}
 }
 
+bool canBeAttacked(char board[BOARD_LENGTH][BOARD_LENGTH],int x, int y) {
+	/* Check current space for Queen */
+	if(board[y][x] == 'Q')
+		return true;
+
+	/* Check Vertical for other Queens */	
+	int tempY;
+	for(tempY = 0; tempY < BOARD_LENGTH; tempY++) {
+		if(board[tempY][x] == 'Q')
+			return true;
+	}
+
+	/* Check Horizontal for other Queens */
+	int tempX;
+	for(tempX = 0; tempX < BOARD_LENGTH; tempX++) {
+		if(board[y][tempX] == 'Q')
+			return true;
+	}
+	
+	/* Check Diagnals for other Queens */	
+	tempX = x;tempY = y;
+	while(tempX >= 0 && tempY >= 0) {
+		if(board[tempY][tempX] == 'Q') {
+			return true;
+		}
+		tempX--;tempY--;
+	}
+	
+	tempX = x;tempY = y;
+	while(tempX < BOARD_LENGTH && tempY < BOARD_LENGTH) {
+		if(board[tempY][tempX] == 'Q') {
+			return true;
+		}
+		tempX++;tempY++;
+	}
+
+			
+	tempX = x;tempY = y;
+	while(tempX < BOARD_LENGTH && tempY >= 0) {
+		if(board[tempY][tempX] == 'Q') {
+			return true;
+		}
+		tempX++;tempY--;
+	}	
+
+		
+	tempX = x;tempY = y;
+	while(tempX >= 0 && tempY < BOARD_LENGTH) {
+		if(board[tempY][tempX] == 'Q') {
+			return true;
+		}
+		tempX--;tempY++;
+	}		
+	return false;
+}
+/*
+For Manual Queen Placment, makes checks for boundries and attacks automatically
+*/
 bool placeQueen(char board[BOARD_LENGTH][BOARD_LENGTH],int x, int y) {
 	/* Check Boundries */
 	if(x > BOARD_LENGTH - 1 || y > BOARD_LENGTH - 1)
 		return false;
 	if(x < 0 || y < 0)
 		return false;
-
-	/* Check current space for Queen */
-	if(board[x][y] == 'Q')
+	
+	if(canBeAttacked(board,x,y))
 		return false;
 
-	/* Check Vertical for other Queens */	
-	int tempY;
-	for(tempY = 0; tempY < BOARD_LENGTH; tempY++) {
-		if(board[x][tempY] == 'Q')
-			return false;
-	}
-
-	/* Check Horizontal for other Queens */
-	int tempX;
-	for(tempX = 0; tempX < BOARD_LENGTH; tempX++) {
-		if(board[tempX][y] == 'Q')
-			return false;
-	}
-	
-	printf("BEGIN DIAGS");
-	/* Check Diagnals for other Queens */	
-	tempX = x;tempY = y;
-	while(tempX >= 0 && tempY >= 0) {
-		if(board[tempX][tempY] == 'Q') {
-			return false;
-		}
-		x--;y--;
-	}	
-	printf("Finish UL");
-	
-	tempX = x;tempY = y;
-	while(tempX < BOARD_LENGTH && tempY < BOARD_LENGTH) {
-		if(board[tempX][tempY] == 'Q') {
-			return false;
-		}
-		x++;y++;
-	}	
-	printf("Finish LR");	
-
 	/* Place Queen Sucessfully */
-	board[x][y] = 'Q';
+	board[y][x] = 'Q';
 	QUEENS_PLACED++;
 	printf("Queen Placed @ (%d,%d)\nTotal Queens Placed: %d\n",x,y,QUEENS_PLACED);
 	return true;
 }
+/*
+To be used for Recursive Queen Placment, assumes in bounds and checks for attacks were already made.
+*/
+void placeQueenR(char board[BOARD_LENGTH][BOARD_LENGTH],int x, int y) {
+	board[y][x] = 'Q';
+	QUEENS_PLACED++;
+	printf("Queen Placed @ (%d,%d)\nTotal Queens Placed: %d\n",x,y,QUEENS_PLACED);
+}
 
 bool queens(char board[BOARD_LENGTH][BOARD_LENGTH], int x, int y) {
-	if(x == BOARD_LENGTH) {
-		printSolution(board);
+	/* Base Case */
+	if(x >= BOARD_LENGTH)
 		return true;
-	}
-	int iY;
-	bool placedQueen = false;
-	for(iY = 0; iY < BOARD_LENGTH; iY++) {
-		if(placeQueen(board,x,iY)) {
-			queens(board,++x,0);
-			return true;
+ 	
+	int row;
+	for(row = 0; row < BOARD_LENGTH; row++) {
+		if(canBeAttacked(board,row,x)) {
+			placeQueen(board,row,x);
+			if(queens(board,row,x+1))
+				return true;
+			board[row][x] = BLANK_SPACE;
 		}
 	}
-	
 }
 
 void printSolution(char board[BOARD_LENGTH][BOARD_LENGTH]) {
 	int x,y;
-	for(x = BOARD_LENGTH - 1; x >= 0; x--) {
-		for(y = 0;  y < BOARD_LENGTH; y++) {
-			if(board[x][y] == 'Q') {
+	for(y = BOARD_LENGTH - 1; y >= 0; y--) {
+		for(x = 0;  x < BOARD_LENGTH; x++) {
+			if(board[y][x] == 'Q') {
 				int correctedY = BOARD_LENGTH - y;
 				printf("%c%d",'a' + x, correctedY);
 			}			
