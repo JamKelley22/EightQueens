7825e741bbd07b94a5d78514d7266f0147603abc
Jameel Kelley <jamkelley22@gmail.com>
2018-01-12 00:32:09 -0600

    Fixed stupid error in attack check, Found one solution sucessfully!

--- queens.c
+++ queens.c
@@ -5,9 +5,6 @@
 const int BOARD_LENGTH = 8;
 const char BLANK_SPACE = '-';
 int QUEENS_PLACED = 0;
-int QUEENS_TO_PLACE = 8;
-int SOLUTIONS_FOUND = 0;
-const int NUM_SOLUTIONS = 92;
 
 void clearBoard(char a[BOARD_LENGTH][BOARD_LENGTH]) {
 	int i,j;
@@ -20,22 +17,22 @@ void clearBoard(char a[BOARD_LENGTH][BOARD_LENGTH]) {
 }
 
 bool canBeAttacked(char board[BOARD_LENGTH][BOARD_LENGTH],int i, int j) {
-	/* Check Horizontal for other Queens */	
-	int tempJ;
-	for(tempJ = 0; tempJ < BOARD_LENGTH; tempJ++) {
-		if(board[i][tempJ] == 'Q')
-			return true;
-	}
-
 	/* Check Vertical for other Queens */
 	int tempI;
 	for(tempI = 0; tempI < BOARD_LENGTH; tempI++) {
 		if(board[tempI][j] == 'Q')
 			return true;
 	}
+
+	/* Check Horizontal for other Queens */	
+	int tempJ;
+	for(tempJ = 0; tempJ < BOARD_LENGTH; tempJ++) {
+		if(board[i][tempJ] == 'Q')
+			return true;
+	}
 	
 	/* Check Diagnals for other Queens */	
-	tempI = 0;tempJ = 0;
+	tempI = i;tempJ = j;
 	while(tempI < BOARD_LENGTH && tempJ < BOARD_LENGTH) {
 		if(board[tempI][tempJ] == 'Q') {
 			return true;
@@ -43,15 +40,84 @@ bool canBeAttacked(char board[BOARD_LENGTH][BOARD_LENGTH],int i, int j) {
 		tempI++;tempJ++;
 	}
 	
-	tempI = BOARD_LENGTH - 1;tempJ = BOARD_LENGTH - 1;
+	tempI = i;tempJ = j;
 	while(tempI >= 0 && tempJ >= 0) {
 		if(board[tempI][tempJ] == 'Q') {
 			return true;
 		}
 		tempI--;tempJ--;
 	}
+
+	
+	tempI = i;tempJ = j;
+	while(tempI < BOARD_LENGTH && tempJ >= 0) {
+		if(board[tempI][tempJ] == 'Q') {
+			return true;
+		}
+		tempI++;tempJ--;
+	}
+	
+	tempI = i;tempJ = j;
+	while(tempI >= 0 && tempJ > BOARD_LENGTH) {
+		if(board[tempI][tempJ] == 'Q') {
+			return true;
+		}
+		tempI--;tempJ++;
+	}
+
 	return false;
 }
+/*
+To be used for Recursive Queen Placment, assumes in bounds and checks for attacks were already made.
+*/
+void placeQueenR(char board[BOARD_LENGTH][BOARD_LENGTH],int i, int j) {
+	board[i][j] = 'Q';
+}
+
+void removeQueenR(char board[BOARD_LENGTH][BOARD_LENGTH],int i, int j) {
+	board[i][j] = BLANK_SPACE;
+}
+
+bool queens(char board[BOARD_LENGTH][BOARD_LENGTH], int j) {
+	/* Base Case */
+	if(j >= BOARD_LENGTH)
+		return true;
+ 	
+	int row; /* The i value */
+	for(row = 0; row < BOARD_LENGTH; row++) {
+		if(!canBeAttacked(board,row,j)) {
+			placeQueenR(board,row,j);
+			if(queens(board,j+1))
+				return true;
+			removeQueenR(board,row,j);
+		}
+	}
+	return false;
+}
+
+void printSolution(char board[BOARD_LENGTH][BOARD_LENGTH]) {
+	printf("===Solution===\n");
+	int i,j;
+	for(i = 0; i < BOARD_LENGTH; i++) {
+		for(j = 0; j < BOARD_LENGTH; j++) {
+			if(board[i][j] == 'Q')
+				printf("(%d,%d) | ", i,j);
+		}
+	}
+	/*
+	for(i = 0; i < BOARD_LENGTH; i++) {
+		for(j = 0; j < BOARD_LENGTH; j++) {
+			if(board[i][j] == 'Q') {
+				printf("%c%d",'a' + j, i);
+			}			
+		}
+
+	}
+	*/
+	printf("\n");	
+}
+
+
 /*
 For Manual Queen Placment, makes checks for boundries and attacks automatically
 */
@@ -70,46 +136,6 @@ bool placeQueen(char board[BOARD_LENGTH][BOARD_LENGTH],int i, int j) {
 	QUEENS_PLACED++;
 	printf("Queen Placed @ (%d,%d)\nTotal Queens Placed: %d\n",i,j,QUEENS_PLACED);
 	return true;
-}
-/*
-To be used for Recursive Queen Placment, assumes in bounds and checks for attacks were already made.
-*/
-void placeQueenR(char board[BOARD_LENGTH][BOARD_LENGTH],int i, int j) {
-	board[i][j] = 'Q';
-	QUEENS_PLACED++;
-}
-
-bool queens(char board[BOARD_LENGTH][BOARD_LENGTH], int j) {
-	/* Base Case */
-	if(j >= BOARD_LENGTH)
-		return true;
- 	
-	int row; /* The i value */
-	for(row = 0; row < BOARD_LENGTH; row++) {
-		if(!canBeAttacked(board,row,j)) {
-			placeQueenR(board,row,j);
-			if(queens(board,j+1))
-				return true;
-			board[row][j] = BLANK_SPACE;
-		}
-	}
-	return false;
-}
-
-void printSolution(char board[BOARD_LENGTH][BOARD_LENGTH]) {
-	printf("===Solution===\n");
-	int i,j;
-	i = 0;
-	j = 0;
-	for(i = 0; i < BOARD_LENGTH; i++) {
-		for(j = 0; j < BOARD_LENGTH; j++) {
-			if(board[i][j] == 'Q') {
-				printf("%c%d",'a' + j, i);
-			}			
-		}
-
-	}
-	printf("\n");	
 }
 
 int main(int argc, char *argv[]) {
